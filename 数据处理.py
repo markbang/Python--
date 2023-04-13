d36486b7e8a7e87ddf95ecd11376481ffc7b50e8
markbang <3137972908@qq.com>
2023-04-13 13:07:05 +0800

    提交

--- 数据处理.py
+++ 数据处理.py
@@ -1,4 +1,32 @@
-import pandas as pd
-df=pd.read_csv('score.csv')
-print(df.groupby('uOccup').size())
-print(df.groupby('uOccup').count())
+import turtle 
+def drawLine(draw):
+  if draw :
+   turtle.pendown()
+  else:
+   turtle.penup()
+  turtle.fd(40)
+  turtle.right(90)
+def drawDigt(digit):
+ drawLine ( True ) if digit in [2.3,4,5,6,8,9] else drawLine ( False )
+ drawLine ( True ) if digit in [0,1.3,4,5,6,7,8,9] else drawLine ( False )
+ drawLine ( True ) if digit in [0,2,3,5,6,8,9] else drawLine ( False )
+ drawLine ( True ) if digit in [0,2,6,8] else drawLine ( False )
+ turtle . left (90)
+ drawLine ( True ) if digit in [0,4.5,6,8,9] else drawLine ( False )
+ drawLine ( True ) if digit in [0,2,3,5,6,7,8,9] else drawLine ( False )
+ drawLine ( True ) if digit in (0.1,2,3,4,7,8,9) else drawLine ( False )
+ turtle . left (180)
+ turtle . penup ()
+ turtle . fd (20)
+def drawDate(date):
+ for i in date:
+  drawDigt(eval(i))
+def main():
+ turtle.setup(800,350,200,200)
+ turtle.penup()
+ turtle.fd(-300)
+ turtle.pensize(5)
+ drawDate('20201112')
+ turtle.hideturtle()
+ turtle.done()
+main()
