f9fc635cd1313e226ee9abee97aab04acc3d3046
markbang <3137972908@qq.com>
2023-04-27 15:14:35 +0800

    tijiao

--- 语法使用.py
+++ 语法使用.py
@@ -1,3 +1,11 @@
-tf=open('haha.txt','r',encoding='utf-8')
-print(tf.readline())
-tf.close()
+import time
+print('正在开始读取文件......')
+time.sleep(3)
+f=open('haha.txt','r',encoding='utf-8')
+for i in f.readlines():
+    print(i.strip())
+print('读取完成！')
+print('请等待几秒关闭文件！')
+time.sleep(3)
+f.close()
+print('文件关闭成功！')
