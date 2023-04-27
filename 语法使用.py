d952da556eb50294b6f2ccfbd4ba37a2b87c9032
markbang <3137972908@qq.com>
2023-04-27 14:02:41 +0800

    tijiao

--- 语法使用.py
+++ 语法使用.py
@@ -1,5 +1,3 @@
-while True:
-    d={1:'hello',2:'world',3:'fine',4:'hahha'}
-    e=input('请输入一个键：')
-    e=eval(e)
-    print(d.get(e,'输入的健不存在！！！'))
+tf=open('haha.txt','r',encoding='utf-8')
+print(tf.readline())
+tf.close()
