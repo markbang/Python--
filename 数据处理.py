0d3aed44690488adc39a93e8b053f2edc6924d8d
markbang <3137972908@qq.com>
2023-05-01 17:14:36 +0800

    tijiao

--- 数据处理.py
+++ 数据处理.py
@@ -1,8 +1,8 @@
-import wordcloud
 import jieba
+import wordcloud
 c=wordcloud.WordCloud
 f=open("hamlet.txt","r",encoding='utf-8').read()
-#txt=jieba.lcut(f)
+txt=jieba.lcut(f)
 w=c(width=1000,font_path='msyh.ttc',height=700,
     background_color='white')
 w.generate(f)
--- 词频、词云/词云生成.py
+++ 词频、词云/词云生成.py
@@ -1,10 +1,10 @@
 import jieba
 import wordcloud
-f=open(r'C:\Users\LENOVO\Desktop\Python文件\词频、词云\20大报告.txt','r',encoding='utf-8')
-t=f.read()
+f = open(r'C:\Users\LENOVO\Desktop\Python文件\词频、词云\20大报告.txt', 'r', encoding='utf-8')
+t = f.read()
 f.close()
-ls=jieba.lcut(t)
-txt=' '.join(ls)
-w=wordcloud.WordCloud(font_path='msyh.ttc',width=1000,height=700,background_color='white')
+ls = jieba.lcut(t)
+txt = ' '.join(ls)
+w = wordcloud.WordCloud(font_path='msyh.ttc', width=1000, height=700, background_color='white')
 w.generate(txt)
 w.to_file('grwprdcloud.png')
