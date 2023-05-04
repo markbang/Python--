4101ce659b8f4cc53245303cd15a58c3d932d6aa
markbang <3137972908@qq.com>
2023-05-04 14:56:51 +0800

    Changes

--- 语法使用.py
+++ 语法使用.py
@@ -1,11 +1,17 @@
-import time
-print('正在开始读取文件......')
-time.sleep(3)
-f=open('haha.txt','r',encoding='utf-8')
-for i in f.readlines():
-    print(i.strip())
-print('读取完成！')
-print('请等待几秒关闭文件！')
-time.sleep(3)
-f.close()
-print('文件关闭成功！')
+#  自定义函数，设置固定次数的登录
+#  由键盘输入密码，密码正确显示“Login success！”，错误显示“Wrong password or invalid input”，并显示剩余输入次数
+#  三次错误，显示“Your account has been suspended”，并退出程序
+def login():
+    for i in range(3):
+        password = input("请输入密码：")
+        if password == "123456":
+            print("Login success！")
+            break
+        else:
+            print("Wrong password or invalid input")
+            print("剩余输入次数：{}".format(2 - i))
+    print("Your account has been suspended")
+    exit()
+
+
+login()
