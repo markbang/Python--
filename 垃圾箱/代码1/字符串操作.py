7070f54d8b914cdc6a5034cc878937b2c91c305b
markbang <3137972908@qq.com>
2023-05-18 13:31:04 +0800

    Changes

--- 垃圾箱/代码1/字符串操作.py
+++ 垃圾箱/代码1/字符串操作.py
@@ -1,9 +1,11 @@
-# 将csv文件中的评论提取出来
-import pandas as pd
-import jieba
-clon = ['地点','评论']
-df = pd.read_csv(r'C:\Users\LENOVO\Desktop\Python文件\项目爬虫\详细信息(包括电影评价人数、评分等)\电影评论\哪吒之魔童降世评论.csv',
-                 encoding='utf-8',names=clon)
-comment = df['评论'].tolist()
-str_comment = ''.join(comment)
-print(str_comment)
+txt = 'If the implementation is eAsy to exPLAin, it may be a good idea.'
+
+
+def bianxiao(text):
+    for index, char in enumerate(text):
+        if text[index-1].isalpha() and char.isupper() and text[index+1].isalpha():
+            text = text.replace(char, char.lower())
+    return text
+
+
+print(bianxiao(txt))
--- 第三方库学习/PyAutoGUI脚本/泰拉瑞亚/自动钓鱼.py
+++ 第三方库学习/PyAutoGUI脚本/泰拉瑞亚/自动钓鱼.py
@@ -4,10 +4,4 @@ import time
 import random
 
 width, height = pyautogui.size()
-currentMouseX, currentMouseY = pyautogui.position()
-for i in range(2):
-    pyautogui.moveTo(100, 100, duration=0.25)
-    pyautogui.moveTo(200, 100, duration=0.25)
-    pyautogui.moveTo(200, 200, duration=0.25)
-    pyautogui.moveTo(100, 200, duration=0.25)
-    pyautogui.moveTo(width/2, height/2, duration=0.25)
+print(width, height)
