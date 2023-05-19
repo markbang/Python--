0a6e9acb3ebf51f32b25ce85824f2e0796194c07
markbang <3137972908@qq.com>
2023-05-19 16:46:24 +0800

    提交

--- 第三方库学习/PyAutoGUI脚本/泰拉瑞亚/自动钓鱼.py
+++ 第三方库学习/PyAutoGUI脚本/泰拉瑞亚/自动钓鱼.py
@@ -2,6 +2,10 @@ import pyautogui
 from PIL import Image
 import time
 import random
-
-width, height = pyautogui.size()
-print(width, height)
+time.sleep(2)
+pyautogui.moveTo(1440, 800, duration=0.25)
+pyautogui.click(1440,800,button='left')
+time.sleep(0.5)
+while True:
+    if not pyautogui.pixelMatchesColor(1376,864,(60,52,56),tolerance=10):
+        pyautogui.doubleClick()
